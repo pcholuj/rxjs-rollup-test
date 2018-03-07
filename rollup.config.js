38fd8b4037823729337b36c4b3a8b9eedd899518
pcholuj <piotr.choluj@filestack.com>
2018-03-07 15:13:14 +0100

    add gzip and uglifyu

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,51 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+import gzip from "rollup-plugin-gzip";
 
-export default {
-  entry: 'main.js',
-  dest: 'bundle.js',
-  format: 'iife',
-  plugins: [
-    resolve({ jsnext: true, main: true }),
-    commonjs(),
-  ],
-};
+export default [{
+	entry: 'main.js',
+	dest: 'bundle.min.js',
+	plugins: [
+		resolve({
+			jsnext: true,
+			main: true
+		}),
+		commonjs(),
+		uglify(),
+		gzip(),
+	],
+}, {
+	entry: 'main.js',
+	dest: 'bundle.min.js',
+	plugins: [
+		resolve({
+			jsnext: true,
+			main: true
+		}),
+		commonjs(),
+		uglify(),
+		gzip(),
+	],
+}, {
+	entry: 'main.js',
+	dest: 'bundle.js',
+	plugins: [
+		resolve({
+			jsnext: true,
+			main: true
+		}),
+		commonjs(),
+	],
+}, {
+	entry: 'main.js',
+	dest: 'bundle.js.gz',
+	plugins: [
+		resolve({
+			jsnext: true,
+			main: true
+		}),
+		commonjs(),
+		gzip(),
+	],
+}];
